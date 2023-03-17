110754a26cce3f5a75c3eaf398a28b3fcf1fea1e
Kirill Kniga <kiruha_95@icloud.com>
2023-03-17 17:30:44 +0300

    Work with model

--- RandomAnimation/Model.swift
+++ RandomAnimation/Model.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Kirill Kniga on 17.03.2023.
 //
+import Foundation
+import SpringAnimation
 
 struct AnimationCollection {
     var preset: String
@@ -13,72 +15,14 @@ struct AnimationCollection {
     var delay: Double
 }
 
-let presetArray = [
-    "pop",
-    "slideLeft",
-    "slideRight",
-    "slideDown",
-    "slideUp",
-    "squeezeLeft",
-    "squeezeRight",
-    "squeezeDown",
-    "squeezeUp",
-    "fadeIn"
-]
-
-let curveArray = [
-    "easeOutQuad",
-    "easeInOutQuad",
-    "easeInCubic",
-    "easeOutCubic",
-    "easeInOutCubic",
-    "easeInQuart",
-    "easeOutQuart",
-    "easeInOutQuart",
-    "easeInQuint",
-    "easeOutQuint"
-]
-
-let forceArray = [
-    1.0,
-    1.05,
-    1.1,
-    1.15,
-    1.2,
-    1.25,
-    1.3,
-    1.35
-]
-
-let durationArray = [
-    1.0,
-    1.05,
-    1.1,
-    1.15,
-    1.2,
-    1.25,
-    1.3,
-    1.35
-]
-
-let delayArray = [
-    0.0,
-    0.2,
-    0.3,
-    0.4,
-    0.5,
-    1.0,
-    1.2
-]
-
 extension AnimationCollection {
     static func getCollection() -> [AnimationCollection] {
         [
-            AnimationCollection(preset: presetArray.randomElement() ?? "ops",
-                                curve: curveArray.randomElement() ?? "ops",
-                                force: forceArray.randomElement() ?? 0.0,
-                                duration: durationArray.randomElement() ?? 0.0,
-                                delay: delayArray.randomElement() ?? 0.0)
+            AnimationCollection(preset: AnimationPreset.allCases.randomElement()?.rawValue ?? "",
+                                curve: AnimationCurve.allCases.randomElement()?.rawValue ?? "",
+                                force: round(Double.random(in: 1.0...1.8) * 10) / 10,
+                                duration: round(Double.random(in: 0.9...1.8) * 10) / 10,
+                                delay: 0.30)
         ]
     }
 }
