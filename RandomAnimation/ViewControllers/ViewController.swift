07667e1449d2a634960584b8ac2a0e04d70ee501
Kirill Kniga <kiruha_95@icloud.com>
2023-03-17 22:47:56 +0300

    Work with logic

--- RandomAnimation/ViewControllers/ViewController.swift
+++ RandomAnimation/ViewControllers/ViewController.swift
@@ -18,21 +18,36 @@ final class ViewController: UIViewController {
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
     
-    @IBOutlet var springButton: SpringButton!
     
-    private var animationCollection = AnimationCollection.getAnimation()
+    private var animationCollection: AnimationCollection {
+        AnimationCollection.getAnimation()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLabels()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func springButtonTapped(_ sender: SpringButton) {
+    @IBAction func runButtonTapped(_ sender: SpringButton) {
         springAnimationView.animate()
-        }
+        sender.setTitle("Run \(animationCollection.preset)", for: .normal)
+        setUpLabels()
+    }
 }
 // MARK: extension ViewController
 extension ViewController {
-    // MARK: viewlabels setup
-    
+    private func setUpLabels() {
+        presetLabel.text = "Preset: \(animationCollection.preset)"
+        curveLabel.text = "Curve: \(animationCollection.curve)"
+        forceLabel.text = "Force: \(animationCollection.force)"
+        durationLabel.text = "Duration: \(animationCollection.duration)"
+        delayLabel.text = "Delay: \(animationCollection.delay)"
+        
+        springAnimationView.animation = animationCollection.preset
+        springAnimationView.curve = animationCollection.curve
+        springAnimationView.force = animationCollection.force
+        springAnimationView.duration = animationCollection.duration
+        springAnimationView.delay = animationCollection.delay
+    }
 }
